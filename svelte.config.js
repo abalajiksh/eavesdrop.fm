6ed7c89ceae26913a8783ace0e0ac708c2fbde6a
Ashwin Balaji <125642240+abalajiksh@users.noreply.github.com>
2025-12-27 18:49:02 +0100

    Fix: Simplify config to use only svelte-preprocess with TypeScript enabled

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import sveltePreprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Use both vitePreprocess and sveltePreprocess for SCSS support
-	preprocess: [
-		vitePreprocess(),
-		sveltePreprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
-	],
+	preprocess: preprocess({
+		scss: {
+			prependData: '@use "src/variables.scss" as *;'
+		},
+		typescript: true
+	}),
 
 	kit: {
 		adapter: adapter()
